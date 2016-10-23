b418d50a449c794a9fb3c311d727b5ead7d683a8
Hynnej <hackworj@oregonstate.edu>
2016-10-23 15:25:52 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+var_dump($method);
 var_dump($request);
 $doc = preg_replace('/[^a-z0-9_]+/i','', array_shift($request));
 
