d3a4c7e9afe066aaf718c76156708b7f2245d801
Hynnej <hackworj@oregonstate.edu>
2016-10-23 14:55:19 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,20 @@
 <?php
-phpinfo();
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+var_dump($request);
+$doc = preg_replace('/[^a-z0-9_]+/i','', array_shift($request));
+
+if($method == "GET")
+{
+	if($doc == "team")
+	{	
+		$name = explode('=', $request[0]);
+		echo $name[0] . " " . $name[1];
+	}
+	
+	if($doc == "player")
+}
+
+
 ?>
+
