2ee5478ba97cf9c7bb45713c249fffe09ef74e92
Hynnej <hackworj@oregonstate.edu>
2016-10-23 20:37:12 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 
 	//connects to mongodb hosted at mlabs
 	$uri = "mongodb://sirmiq:door5454@ds048319.mlab.com:48319/sports";
-	$client = new MongoDB\Client($uri);
+	$client = new \MongoClient($uri);
 	$db = $client->selectDB('sports');
 	$teams = $db->selectCollection('team');
 	//$players = $db->player;
