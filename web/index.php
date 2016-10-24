11cb5d7f4c521f15f39e488424e91efc0d840292
Hynnej <hackworj@oregonstate.edu>
2016-10-23 20:13:07 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,8 +2,8 @@
 	// require 'vendor/autoload.php';
 
 	//connects to mongodb hosted at mlabs
-	$uri = "mongodb://sirmiq:door5454@ds048719.mlab.com:48719/playerteam";
-	$client = new MongoClient($uri);
+	//$uri = "mongodb://sirmiq:door5454@ds048719.mlab.com:48719/playerteam";
+	$client = new MongoClient('MONGODB_URI');
 	$db = $client->selectDB("playerteam");
 	$teams = $db->team;
 	$players = $db->player;
