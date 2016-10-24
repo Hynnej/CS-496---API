66ae14b117bfa5bd4247c31ff7a1f814fe8e6468
Hynnej <hackworj@oregonstate.edu>
2016-10-23 19:09:50 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -23,7 +23,7 @@
 	
 	$data = array_combine($key, $value);
 	
-	if($method == "GET")
+	/*if($method == "GET")
 	{
 		if($doc == "team")
 		{	
@@ -45,7 +45,7 @@
 		
 		else
 			echo "you must specify a correct collection."
-	}
+	}*/
 	
 /*	else if($method == "POST")
 	{
